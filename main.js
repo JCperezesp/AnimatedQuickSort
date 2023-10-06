c70f88d9953ac38a554d88cc87d7b2dc2d9ae9f6
JCperezesp <juancarlos.pe@usal.es>
2023-10-06 14:01:58 +0200

    com

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const app = Vue.createApp({
   data() {
     return {
-      message : "VUE33",
-      elements : [89,45,15,33,7,24,71,3],
+      message : "ORDENAR",
+      elements : [89,45,15,33,7,24,71,3, 12, 64],
       ThemeFA : {
         class : "fa-solid fa-circle-half-stroke fa-xl",
         type : 1
@@ -18,11 +18,14 @@ const app = Vue.createApp({
       if(ThemeFA.type === 1) {
         ThemeFA.class = "fa-solid fa-circle-half-stroke fa-flip-horizontal fa-xl"
         ThemeFA.type = 0;
+        document.body.style.backgroundColor = "#FEFEFE"
       }
       else {
         ThemeFA.class = "fa-solid fa-circle-half-stroke fa-xl";
         ThemeFA.type = 1;
+        document.body.style.backgroundColor = "#1a1a2a"
       }
+
     }
   },
 })
