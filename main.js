cdd5b43d06a0976f21ca67cffd7a3302308137b8
Juan Carlos <juancarlos.pe@usal.es>
2023-11-09 19:42:57 +0100

    comp props

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const app = Vue.createApp({
   data() {
     return {
-      message : "VUE33",
+      message : "REGENERAR VECTOR",
       elements : [89,45,15,33,7,24,71,3],
+      ArrayLength : 8,
       ThemeFA : {
         class : "fa-solid fa-circle-half-stroke fa-xl",
         type : 1
@@ -10,7 +11,36 @@ const app = Vue.createApp({
     }
   },
   methods: {
-    quickSortArray() {
+    regenVector() {
+      this.elements = []
+      for(let i = 0; i < this.ArrayLength; i++) 
+      //Generate random array of numbers with ArrayLength elements
+        this.elements.push(Math.floor(Math.random() * 120) + 5)
+    },
+    colocar(elements, left, right) {
+      let iP = left;
+      const pivot = elements[iP];
+      for(let i = left + 1; i<= right; i++) {
+        if(elements[i] < pivot) {
+          iP++;
+          let temp = elements[iP]
+          elements[iP] = elements[i]
+          elements[i] = temp
+        }
+      }
+      let temp = elements[left]
+      elements[left] = elements[iP]
+      elements[iP] = temp;
+      console.log("ELEMENTS vale: ", elements);
+      return iP;
+    },
+    quickSortArray(elements, left, right) {
+      if(left < right) {
+         let iP = this.colocar(elements, left, right);
+         this.quickSortArray(elements, left, iP - 1);
+         this.quickSortArray(elements, iP + 1, right)
+      }
+     
       
     },
     switchTheme(ThemeFA) {
